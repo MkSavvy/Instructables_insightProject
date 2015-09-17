af7e09ddfb920711043daf47cdea9fc634a9ab6a
Michel.Savard <no@address.yet>
2015-09-17 19:56:46 -0400

    spider strategy for a crawler linking to project page, NOT WORKING YET

--- DIYprojects/DIYprojects/spiders/instructables_spyder.py
+++ DIYprojects/DIYprojects/spiders/instructables_spyder.py
@@ -7,26 +7,33 @@ Created on Thu Sep 17 12:16:20 2015
 
 import scrapy
 
+from DIYprojects.items import DiyprojectsItem
+
 class DIYSpider(scrapy.Spider):
     name = "diySpider"
     allowed_domains = ["instructables.com"]
     start_urls = [
         "http://www.instructables.com/tag/type-id/category-technology/"
     ]
+    
 
     def parse(self, response):
         for sel in response.selector.xpath("//div[@class='cover-info ']/span[@class='title']"): 
-            url= sel.xpath("a/@href")
-            yield response.Request(url, callback=self.parse_tech_page)
+            hyperREF= sel.xpath("a/@href")
+            allstepAPIkey = "?ALLSTEPS"
+            url = response.urljoin(hyperREF.extract()) + allstepAPIkey
+            print url
+            yield scrapy.Request(url, callback=self.parse_tech_page)
         
 
-
-#   def parse_tech_page(self, response):
-#        for sel in response.xpath('//ul/li'):
-#            item =   DomzItem()          
-#            item["title"] = sel.xpath('a/text()').extract()
+    def parse_tech_page(self, response):
+        for sel in response.xpath('//div[@id="instructable-steps"]'):
+            print sel.url
+#            item = DiyprojectsItem()          
+#            item["projectURL"] = sel.xpath('a/text()').extract()
 #            item["lists"] = sel.xpath('a/@href').extract()
-#            item["author"] = sel.xpath('text()').extract()
+#            item["views"] = sel.xpath('text()').extract()
+#            item["favorites"] = sel.xpath('text()').extract()
 #            yield scrapy.Request(url, self.parse_articles_follow_next_page)      
 #        
 #        
