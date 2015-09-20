f4ee7760f847bdcac642cdb1458fb8998c8ef645
Michel.Savard <no@address.yet>
2015-09-20 14:57:30 -0400

    New crawler initial commit for page content

--- DIYcontent/DIYcontent/spiders/content_spider.py
+++ DIYcontent/DIYcontent/spiders/content_spider.py
@@ -10,28 +10,36 @@ import scrapy
 from DIYcontent.items import DiycontentItem
 
 class contentSpider(scrapy.Spider):
-    name = "diySpider"
+    name = "diycontent"
     allowed_domains = ["instructables.com"]
     start_urls = [
         "http://www.instructables.com/id/Musical-RGB-Lamp/?ALLSTEPS", 
-        "http://www.instructables.com/id/System-For-Automatically-Limiting-TV-Time/",
-        "http://www.instructables.com/id/Raspberry-Pi-Based-Wireless-Microphone/"
+        "http://www.instructables.com/id/System-For-Automatically-Limiting-TV-Time/?ALLSTEPS",
+        "http://www.instructables.com/id/Raspberry-Pi-Based-Wireless-Microphone/?ALLSTEPS"
     ]
     
     def parse(self, response):
         item = DiycontentItem()
-        item["projectID"] = response.selector.xpath("")
-        item["title"] = response.selector.xpath("")
-#       item["author"]  = scrapy.Field()
-#       item["date"]  = scrapy.Field()
-#       item["keywords"]  = scrapy.Field()
-#       item["award"]  = scrapy.Field()    
+        item["title"] = response.selector.xpath("//head/title/text()").extract()
+        item["projectID"] = response.url
+        
+        head_path = response.selector.xpath("//div[@class='header-bar']")
+        item["date"]  = head_path.xpath("./meta[@itemprop='datePublished']/@content").extract()
+        item["keywords"]  =  head_path.xpath("./meta[@itemprop='keywords']/@content").extract()
+        item["views"]  =  head_path.xpath("./meta[@itemprop='interactionCount']/@content").extract()
+        
+        item["author"]  = response.selector.xpath("//span[@class='author']/text()").extract()
+        
+        body_path = response.selector.xpath("//div[@id='main-content']")        
+        item["descr"] = body_path.xpath(".//div[@class='txt step-body']/p/text()").extract()
+        item["scripts"] = body_path.xpath(".//div[@class='txt step-body']/pre/text()").extract()
+        item[""] Nsteps = scrapy.Field()
 #       item[""] tags = scrapy.Field()
+        yield item
+#               item["award"]  = scrapy.Field()   
 #       item[""]relatedURL = scrapy.Field()
-#       item[""]descr = scrapy.Field()
 #       item[""]comments = scrapy.Field()
 #       item[""] tools = scrapy.Field()
-#       item[""] Nsteps = scrapy.Field()
 #       item[""] Nimages = scrapy.Field()
 #       item[""] Nvideos = scrapy.Field()
 #       item[""] Nlink = scrapy.Field()
